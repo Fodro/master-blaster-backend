af5be69960b700989a9c81ca0dfabc0a662a41bc
fodro <feodor.tomilov@yandex.ru>
2022-10-01 13:16:37 +0300

    feat: prepared base
    * inited db_setup
    * refactored code to async style

--- auth/auth.py
+++ auth/auth.py
@@ -16,25 +16,25 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 users_db = UsersDB()
 
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
+async def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def hash_password(plain_password: str) -> str:
+async def hash_password(plain_password: str) -> str:
     return pwd_context.hash(plain_password)
 
 
-def authentication(username: int, password: str) -> User.Model:
-    user = users_db.get_user_by_username(username)
+async def authentication(username: int, password: str) -> User.Model:
+    user = await users_db.get_user_by_username(username)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    if not verify_password(password, user.hashed_password):
+    if not await verify_password(password, user.hashed_password):
         raise HTTPException(status_code=403, detail="Wrong credentials")
     return user
 
 
 # DO NOT USE DEFAULT VALUE
-def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=3)) -> str:
+async def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=3)) -> str:
     data_to_encode = data.copy()
     expire = datetime.utcnow() + expires_delta
     data_to_encode.update({"expires": expire.strftime("%Y-%m-%d %H:%M:%S")})
@@ -42,7 +42,7 @@ def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes
     return encoded_jwt
 
 
-def get_current_user(token: str = Depends(oauth2_scheme)) -> User.Model:
+async def get_current_user(token: str = Depends(oauth2_scheme)) -> User.Model:
     credentials_exception = HTTPException(
         status_code=401,
         detail="Could not validate credentials",
@@ -56,17 +56,17 @@ def get_current_user(token: str = Depends(oauth2_scheme)) -> User.Model:
         token_data = TokenData.Model(user_id=user_id)
     except JWTError:
         raise credentials_exception
-    user = users_db.get_user_by_id(token_data.user_id)
+    user = await users_db.get_user_by_id(token_data.user_id)
     if user is None:
         raise credentials_exception
     return user
 
 
-def create_new_user(user: User.SignUpModel) -> Token.Model:
-    hashed_password = hash_password(user.plain_password)
-    new_user = users_db.create_new_user(User.Model(id=1, username=user.username,
-                                        email=user.email, hashed_password=hashed_password))
-    access_token = create_access_token(
+async def create_new_user(user: User.SignUpModel) -> Token.Model:
+    hashed_password = await hash_password(user.plain_password)
+    new_user = await users_db.create_new_user(User.Model(id=1, username=user.username,
+                                                         email=user.email, hashed_password=hashed_password))
+    access_token = await create_access_token(
         data={"sub": str(new_user.id)},  # expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
--- db/users.py
+++ db/users.py
@@ -12,20 +12,20 @@ class UsersDB:
             hashed_password="$2b$12$h0HEeJkRlK.J3JtnoipyQOstVLLozCjq3n4zNOE4JwCFfEA0k9kw6"
         ))
 
-    def get_user_by_id(self, id: int) -> User.Model:
+    async def get_user_by_id(self, id: int) -> User.Model:
         for user in self.users:
 
             if user.id == id:
                 return user
         raise HTTPException(status_code=404, detail="User not found")
 
-    def get_user_by_username(self, username: str) -> User.Model:
+    async def get_user_by_username(self, username: str) -> User.Model:
         for user in self.users:
             if user.username == username:
                 return user
         raise HTTPException(status_code=404, detail="User not found")
 
-    def create_new_user(self, user: User.Model) -> bool:
+    async def create_new_user(self, user: User.Model) -> bool:
         for item in self.users:
             if item.username == user.username or item.email == user.email:
                 raise HTTPException(
